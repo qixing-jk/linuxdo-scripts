37109b1d4c32d32ccc6ce7cbbdff1462cbbda2dc
anghunk <65840178+anghunk@users.noreply.github.com>
2026-01-30 15:31:04 +0800

    feat(docs): 添加 GitHub Star 组件
    - 在 VitePress 文档中添加 GitHub Star 按钮组件，用于显示项目 Star 数量。
    - 更新主题配置以集成新组件。
    - 优化代码格式和结构，提高可读性。

--- docs/docs/.vitepress/theme/index.js
+++ docs/docs/.vitepress/theme/index.js
@@ -1,9 +1,10 @@
 import DefaultTheme from 'vitepress/theme';
 import giscusTalk from 'vitepress-plugin-comment-with-giscus';
 import { useData, useRoute } from 'vitepress';
-import { toRefs } from "vue";
+import { h } from 'vue';
 
 // 引入组件
+import GitHubStar from '../components/githubStar.vue';
 import './custom.css';
 import Layout from './Layout.vue';
 
@@ -16,56 +17,63 @@ import codeblocksFold from 'vitepress-plugin-codeblocks-fold';
 import 'vitepress-plugin-codeblocks-fold/style/index.scss';
 
 export default {
-  ...DefaultTheme,
-  Layout,
-  enhanceApp(ctx) {
-    DefaultTheme.enhanceApp(ctx);
-  },
-  setup() {
-    const { frontmatter } = useData();
-    const route = useRoute();
-    const initZoom = () => {
-      mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
-    }
-    onMounted(() => {
-      initZoom()
-    })
-    watch(
-      () => route.path,
-      () => nextTick(() => initZoom())
-    )
+	...DefaultTheme,
+	Layout() {
+		const Layout = DefaultTheme.Layout;
+		return h(Layout, null, {
+			'nav-bar-content-after': () => h(GitHubStar),
+		});
+	},
+	enhanceApp(ctx) {
+		DefaultTheme.enhanceApp(ctx);
+	},
+	setup() {
+		const { frontmatter } = useData();
+		const route = useRoute();
+		const initZoom = () => {
+			mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
+		};
+		onMounted(() => {
+			initZoom();
+		});
+		watch(
+			() => route.path,
+			() => nextTick(() => initZoom()),
+		);
 
-    codeblocksFold({ route, frontmatter }, true, 600);
+		codeblocksFold({ route, frontmatter }, true, 600);
 
-
-    // Obtain configuration from: https://giscus.app/
-    giscusTalk({
-      repo: 'anghunk/linuxdo-scripts',
-      repoId: 'R_kgDOMfjPXA',
-      category: 'Announcements', // default: `General`
-      categoryId: 'DIC_kwDOMfjPXM4Chc6M',
-      mapping: 'pathname', // default: `pathname`
-      inputPosition: 'top', // default: `top`
-      lang: 'zh-CN', // default: `zh-CN`
-      // i18n setting (Note: This configuration will override the default language set by lang)
-      // Configured as an object with key-value pairs inside:
-      // [your i18n configuration name]: [corresponds to the language pack name in Giscus]
-      locales: {
-        'zh-Hans': 'zh-CN',
-        'en-US': 'en'
-      },
-      homePageShowComment: true, // Whether to display the comment area on the homepage, the default is false
-      lightTheme: 'preferred_color_scheme', // default: `light`
-      darkTheme: 'transparent_dark', // default: `transparent_dark`
-      // ...
-    }, {
-      frontmatter, route
-    },
-      // Whether to activate the comment area on all pages.
-      // The default is true, which means enabled, this parameter can be ignored;
-      // If it is false, it means it is not enabled.
-      // You can use `comment: true` preface to enable it separately on the page.
-      true
-    );
-  },
-}
+		// Obtain configuration from: https://giscus.app/
+		giscusTalk(
+			{
+				repo: 'anghunk/linuxdo-scripts',
+				repoId: 'R_kgDOMfjPXA',
+				category: 'Announcements', // default: `General`
+				categoryId: 'DIC_kwDOMfjPXM4Chc6M',
+				mapping: 'pathname', // default: `pathname`
+				inputPosition: 'top', // default: `top`
+				lang: 'zh-CN', // default: `zh-CN`
+				// i18n setting (Note: This configuration will override the default language set by lang)
+				// Configured as an object with key-value pairs inside:
+				// [your i18n configuration name]: [corresponds to the language pack name in Giscus]
+				locales: {
+					'zh-Hans': 'zh-CN',
+					'en-US': 'en',
+				},
+				homePageShowComment: true, // Whether to display the comment area on the homepage, the default is false
+				lightTheme: 'preferred_color_scheme', // default: `light`
+				darkTheme: 'transparent_dark', // default: `transparent_dark`
+				// ...
+			},
+			{
+				frontmatter,
+				route,
+			},
+			// Whether to activate the comment area on all pages.
+			// The default is true, which means enabled, this parameter can be ignored;
+			// If it is false, it means it is not enabled.
+			// You can use `comment: true` preface to enable it separately on the page.
+			true,
+		);
+	},
+};
